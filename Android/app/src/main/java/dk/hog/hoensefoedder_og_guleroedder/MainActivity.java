02b02a3fc7635f99f50bb1764200e0442cd31698
mart610k <mart610k@zbc.dk>
2022-11-21 09:27:03 +0100

    Added repo for Andorid

---
+++ Android/app/src/main/java/dk/hog/hoensefoedder_og_guleroedder/MainActivity.java
@@ -0,0 +1,14 @@
+ package dk.hog.hoensefoedder_og_guleroedder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
